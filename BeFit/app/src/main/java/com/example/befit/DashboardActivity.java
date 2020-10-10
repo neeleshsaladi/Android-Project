827bead067de55d11d64019764d50472fba5021e
neeleshsaladi <neelesh.saladi@gmail.com>
2020-10-09 21:15:29 -0500

    Added new classes

--- BeFit/app/src/main/java/com/example/befit/DashboardActivity.java
+++ BeFit/app/src/main/java/com/example/befit/DashboardActivity.java
@@ -2,13 +2,44 @@ package com.example.befit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements OnClickListener {
 
+    Button b1,b2;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_main);
+        b1= (Button) findViewById(R.id.Gym);
+        b2= (Button) findViewById(R.id.BMI);
+
+        b1.setOnClickListener(this);
+        b2.setOnClickListener(this);
+
+    }
+
+
+
+    public void onClick(View v)
+    {
+        if(v.getId()==R.id.Gym)
+        {
+            Intent intent=new Intent(getApplicationContext(),Gym.class);
+            startActivity(intent);
+        }
+        else if (v.getId()==R.id.BMI)
+        {
+            Intent in=new Intent(getApplicationContext(),BMI.class);
+            startActivity(in);
+        }
+
     }
 }
