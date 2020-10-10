a88ecb13122e45473658380c02193728e0619d44
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2020-10-10 17:19:26 -0500

    added ratecard

--- BeFit/app/src/main/java/com/example/befit/RateCard.java
+++ BeFit/app/src/main/java/com/example/befit/RateCard.java
@@ -1,4 +1,17 @@
 package com.example.befit;
 
-public class RateCard {
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class RateCard extends Activity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ratecard);
+    }
+
 }
