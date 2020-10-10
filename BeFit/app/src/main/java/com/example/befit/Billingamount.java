4c3d568933729114ed71fed865e78f52c988b256
neeleshsaladi <neelesh.saladi@gmail.com>
2020-10-10 17:57:51 -0500

    Changes made to billing amount

---
+++ BeFit/app/src/main/java/com/example/befit/Billingamount.java
@@ -0,0 +1,13 @@
+package com.example.befit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Billingamount extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
--- BeFit/app/src/main/java/com/example/befit/RateCard.java
+++ BeFit/app/src/main/java/com/example/befit/RateCard.java
@@ -1,7 +1,10 @@
 package com.example.befit;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class RateCard extends Activity {
@@ -12,6 +15,18 @@ public class RateCard extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ratecard);
+        addListenerOnButton();
+    }
+    public void addListenerOnButton() {
+        final Context context = this;
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, Billingamount.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
