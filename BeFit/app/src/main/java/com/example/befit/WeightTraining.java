223b31976c7dae1bd57155eb732ff2023aecabdc
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2020-10-10 17:24:31 -0500

    added weight training

--- BeFit/app/src/main/java/com/example/befit/WeightTraining.java
+++ BeFit/app/src/main/java/com/example/befit/WeightTraining.java
@@ -1,4 +1,40 @@
 package com.example.befit;
 
-public class WeightTraining {
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WeightTraining extends Activity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weighttraining);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, RateCard.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
 }
