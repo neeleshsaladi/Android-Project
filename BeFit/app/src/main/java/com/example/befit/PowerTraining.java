4f42a611a67256055eb09d6a2020d313577ab47a
Ravichanderreddy-goli <ravichanderreddy73@gmail.com>
2020-10-10 17:07:05 -0500

    added power training

--- BeFit/app/src/main/java/com/example/befit/PowerTraining.java
+++ BeFit/app/src/main/java/com/example/befit/PowerTraining.java
@@ -1,4 +1,40 @@
 package com.example.befit;
 
-public class PowerTraining {
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PowerTraining extends Activity {
+
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.powertraining);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, RateCard.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
 }
