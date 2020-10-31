d0892728b7509c0acfbb2dbcd974ea2eac4c43a3
neeleshsaladi <neelesh.saladi@gmail.com>
2020-10-30 23:37:17 -0500

    Adding the new changes with functionality to the project with a new xml design.

--- BeFit/app/src/main/java/com/example/befit/LoginActivity.java
+++ BeFit/app/src/main/java/com/example/befit/LoginActivity.java
@@ -8,14 +8,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
     Button login,usersignup;
     TextView forgot;
+
+    EditText username,pswrd;
+
+    ArrayList<String> login_list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        login_list.add("Johnalbert@gmail.com");
+        login_list.add("test1@gmail.com");
+        login_list.add("Carolineham@gmail.com");
+        login_list.add("Mikytim@gmail.com");
+        login_list.add("Mikegreg@gmail.com");
+        login_list.add("Chrislunduk@gmail.com");
+        login_list.add("Terryjack@gmail.com");
+        login_list.add("Rohitha21@gmail.com");
+        login_list.add("test@test.com");
+
+
+        username = findViewById(R.id.username);
+        pswrd = findViewById(R.id.pswrd);
+
         login = findViewById(R.id.login);
         forgot = findViewById(R.id.forgot);
         usersignup = findViewById(R.id.usersignup);
@@ -29,8 +52,29 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
-                startActivity(intent);
+                /*Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+                startActivity(intent);*/
+                String password = pswrd.getText().toString();
+                String mailId = username.getText().toString();
+                if(!password.equals("") && !mailId.equalsIgnoreCase("")){
+
+                    for(int i=1;i<login_list.size() ; i++){
+                        if(mailId.equalsIgnoreCase(login_list.get(i))){
+                            Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+                            startActivity(intent);
+                            return;
+                        }
+                        Toast.makeText(getApplicationContext(), "User didn't registred with us.please do new Signup",
+                                Toast.LENGTH_SHORT)
+                                .show();
+                    }
+
+
+                }else {
+                    Toast.makeText(getApplicationContext(), "Please enter Email-id and Password",
+                            Toast.LENGTH_SHORT)
+                            .show();
+                }
             }
         });
         forgot.setOnClickListener(new View.OnClickListener() {
