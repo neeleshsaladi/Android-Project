51a177aa64cd8a69b29276b2d20be830a1afb9e2
Rohitha <rohitha.meda216@gmail.com>
2020-11-18 23:09:56 -0600

    Connected project to firebase

--- BeFit/app/src/main/java/com/example/befit/LoginActivity.java
+++ BeFit/app/src/main/java/com/example/befit/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.befit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,6 +11,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
@@ -17,14 +24,15 @@ public class LoginActivity extends AppCompatActivity {
     TextView forgot;
 
     EditText username,pswrd;
-
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     ArrayList<String> login_list = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        mAuth = FirebaseAuth.getInstance();
         login_list.add("Johnalbert@gmail.com");
         login_list.add("test1@gmail.com");
         login_list.add("Carolineham@gmail.com");
@@ -54,27 +62,46 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 /*Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
                 startActivity(intent);*/
-                String password = pswrd.getText().toString();
-                String mailId = username.getText().toString();
-                if(!password.equals("") && !mailId.equalsIgnoreCase("")){
 
-                    for(int i=1;i<login_list.size() ; i++){
-                        if(mailId.equalsIgnoreCase(login_list.get(i))){
-                            Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
-                            startActivity(intent);
-                            return;
+                mAuth.signInWithEmailAndPassword(username.getText().toString(),pswrd.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"verified",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+//                            if(mAuth.getCurrentUser().isEmailVerified()){
+//
+//                            }
+//                            else{
+//                                Toast.makeText(getApplicationContext(),"failed",Toast.LENGTH_SHORT).show();
+//                            }
                         }
-                        Toast.makeText(getApplicationContext(), "User didn't registred with us.please do new Signup",
-                                Toast.LENGTH_SHORT)
-                                .show();
+                        else{
+                            Toast.makeText(getApplicationContext(),"incorrect",Toast.LENGTH_SHORT).show();
+                        }
                     }
-
-
-                }else {
-                    Toast.makeText(getApplicationContext(), "Please enter Email-id and Password",
-                            Toast.LENGTH_SHORT)
-                            .show();
-                }
+                });
+//                String password = pswrd.getText().toString();
+//                String mailId = username.getText().toString();
+//                if(!password.equals("") && !mailId.equalsIgnoreCase("")){
+//
+//                    for(int i=1;i<login_list.size() ; i++){
+//                        if(mailId.equalsIgnoreCase(login_list.get(i))){
+//                            Intent intent = new Intent(LoginActivity.this,DashboardActivity.class);
+//                            startActivity(intent);
+//                            return;
+//                        }
+//                        Toast.makeText(getApplicationContext(), "User didn't registred with us.please do new Signup",
+//                                Toast.LENGTH_SHORT)
+//                                .show();
+//                    }
+//
+//
+//                }else {
+//                    Toast.makeText(getApplicationContext(), "Please enter Email-id and Password",
+//                            Toast.LENGTH_SHORT)
+//                            .show();
+//                }
             }
         });
         forgot.setOnClickListener(new View.OnClickListener() {
--- BeFit/app/src/main/java/com/example/befit/SignUp.java
+++ BeFit/app/src/main/java/com/example/befit/SignUp.java
@@ -1,23 +1,40 @@
 package com.example.befit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignUp extends AppCompatActivity {
     Button signup;
     EditText firstname,lastname,email,password,cpassword;
-
+    String customerID;
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        mAuth = FirebaseAuth.getInstance();
         cpassword = findViewById(R.id.cpassword);
         password = findViewById(R.id.password);
         email = findViewById(R.id.email);
@@ -42,9 +59,51 @@ public class SignUp extends AppCompatActivity {
                 }else if(!password.getText().toString().equals(cpassword.getText().toString())){
                     Toast.makeText(getApplicationContext(), "Password and Confirm Password didn't match", Toast.LENGTH_SHORT).show();
                 }else {
-                    Intent i = new Intent(SignUp.this,DashboardActivity.class);
-                    startActivity(i);
+                    mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                customerID = mAuth.getCurrentUser().getUid();
+                                DocumentReference documentReference = db.collection("users").document(customerID);
+                                Map<String, Object> user = new HashMap<>();
+                                user.put("firstName",firstname.getText().toString());
+                                user.put("lastName",lastname.getText().toString());
+                                user.put("email",email.getText().toString());
+                                user.put("password",password.getText().toString());
+
+                                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Toast.makeText(getApplicationContext(),"success",Toast.LENGTH_SHORT).show();
+                                        Intent i = new Intent(SignUp.this,LoginActivity.class);
+                                        startActivity(i);
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(getApplicationContext(),"failure",Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+//                                mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+//                                    @Override
+//                                    public void onComplete(@NonNull Task<Void> task) {
+//                                        if(task.isSuccessful()){
+//                                            Toast.makeText(getApplicationContext(),"check the email",Toast.LENGTH_SHORT).show();
+//                                            Intent i = new Intent(SignUp.this,LoginActivity.class);
+//                                            startActivity(i);
+//                                        }
+//                                        else{
+//                                            Toast.makeText(getApplicationContext(),"error",Toast.LENGTH_SHORT).show();
+//                                        }
+//                                    }
+//                                });
+                            }
+                        }
+                    });
+
                 }
+
             }
         });
     }
